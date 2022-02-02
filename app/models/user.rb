2c54ef0c7f0c9f82b75f35dbcc9c97712ada6f5c
EC2 Default User <ec2-user@ip-172-31-4-12.ec2.internal>
2022-02-02 17:30:47 +0000

    [Add]グループ機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :group_users
+  has_many :groups, through: :group_users
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
@@ -12,7 +14,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  
+
   # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
   # 与フォロー関係を通じて参照→自分がフォローしている人
@@ -39,7 +41,7 @@ class User < ApplicationRecord
   def follow?(user)
     followings.include?(user)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @user = User.where("name LIKE?", "#{word}")
