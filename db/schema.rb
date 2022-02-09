7dec386280a2c08959b8cd653cf9add30cb7fc26
EC2 Default User <ec2-user@ip-172-31-4-12.ec2.internal>
2022-02-09 04:35:23 +0000

    [Add]タグ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_132053) do
+ActiveRecord::Schema.define(version: 2022_02_09_035227) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2022_02_06_132053) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_book_tags_on_post_id"
+    t.index ["tag_id"], name: "index_book_tags_on_tag_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -110,6 +119,12 @@ ActiveRecord::Schema.define(version: 2022_02_06_132053) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -133,6 +148,7 @@ ActiveRecord::Schema.define(version: 2022_02_06_132053) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "book_tags", "tags"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
 end
